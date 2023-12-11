8a18b189f0a0f0dc54f7a253dd2afe0b1adf8f34
Uliana Shlagina <shlaginau@gmail.com>
2023-12-11 14:01:25 +0700

    initial commit

---
+++ src/js/app.js
@@ -0,0 +1,9 @@
+/* import GameSavingLoader from './GameSavingLoader';
+
+GameSavingLoader.load()
+  .then((saving) => {
+    console.log('GameSaving loaded:', saving);
+  })
+  .catch((error) => {
+    console.error('Error loading GameSaving:', error);
+  }); */
